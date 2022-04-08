5399829c014d52c306963173455f6ba1a88354c8
Blanca Ley <blanca.ley@outlook.com>
2022-04-08 15:44:55 +0200

    Print books

--- frontend/main.js
+++ frontend/main.js
@@ -5,12 +5,23 @@ const fetchData = async (url) => {
 
 const printBooks = async () => {
     const books = await fetchData("http://localhost:1337/api/books")
-    console.log(books.data)
-}
-printBooks();
+    const booksList = document.getElementById("booksList");
 
+    books.data.forEach(book => {
+      const { attributes: { title, author, rating, pages} } = book;
+      const bookItem = document.createElement("li");
+      bookItem.innerHTML = `<h3>${title}</h3>
+      <p>${author}</p>
+      <p>${rating}</p>
+      <p>${pages}</p>`
+
+      booksList.append(bookItem)
+    });
+}
 const printAudiobooks = async () => {
     const audiobooks = await fetchData("http://localhost:1337/api/audiobooks")
     console.log(audiobooks.data)
 }
+
+printBooks();
 printAudiobooks();
