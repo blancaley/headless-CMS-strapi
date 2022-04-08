2303aad00af780d2c80cd5913579890fe2d5f696
Blanca Ley <blanca.ley@outlook.com>
2022-04-08 15:54:23 +0200

    Log in from app

--- frontend/main.js
+++ frontend/main.js
@@ -3,6 +3,25 @@ const fetchData = async (url) => {
     return data;
 }
 
+const login = async () => {
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+
+  const {data} = await axios.post("http://localhost:1337/api/auth/local", {
+    identifier: "Batman",
+    password: "Batman123"
+    // identifier: username.value,
+    // password: password.value
+  })
+
+  const token = data.jwt;
+  sessionStorage.setItem("token", token);
+
+  //Reset inputs
+  username.value="";
+  password.value="";
+}
+
 const printBooks = async () => {
     const books = await fetchData("http://localhost:1337/api/books")
     const booksList = document.getElementById("booksList");
@@ -25,3 +44,9 @@ const printAudiobooks = async () => {
 
 printBooks();
 printAudiobooks();
+
+// Event Listeners
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  login();
+})
