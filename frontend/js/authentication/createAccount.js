790675455469af035d39aa20881863cabbfba8ad
Blanca Ley <blanca.ley@outlook.com>
2022-04-11 15:59:56 +0200

    Add function to create new user from UI

---
+++ frontend/js/authentication/createAccount.js
@@ -0,0 +1,22 @@
+const signUpBtn = document.getElementById("signUpBtn");
+
+// Create new user and log in
+const createAccount = async () => {
+  const username = document.getElementById("newUsername");
+  const email = document.getElementById("newEmail");
+  const password = document.getElementById("newPassword");
+
+  const response = await axios.post("http://localhost:1337/api/auth/local/register", {    
+    username: username.value,
+    email: email.value,
+    password: password.value,
+  }).then(response => {
+    const token = response.data.jwt;
+    sessionStorage.setItem("token", token);
+  })
+}
+
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  createAccount();
+})
---
+++ frontend/js/main.js
@@ -0,0 +1,39 @@
+const LOCAL_HOST = "http://localhost:1337";
+const libraryList = document.getElementById("libraryList");
+
+const printLibraryList = async () => {
+  const books = await getBooks();
+  const audiobooks = await getAudiobooks();
+
+  books.forEach(book => {
+    printBookCard(book);
+  });
+
+  audiobooks.forEach(audiobook => {
+    printAudiobookCard(audiobook);
+  });
+}
+
+const printBookCard = async (book) => {
+  const { attributes: { title, author, rating, pages:pagesNum, cover: { data: { attributes: {url}}}}} = book;
+
+  const bookItem = document.createElement("article");
+  bookItem.innerHTML = 
+    `<div id="bookOwner">
+    <p>Batman</p>
+    <p>batman@gotham.com</p>
+    </div>
+    <div id="bookDetails">
+      <img id="bookCover" src="${LOCAL_HOST}${url}" alt="${title} book cover">
+      <h2>${title}</h2>
+      <p>By ${author}</p>
+      <p>Book <span>(${pagesNum} pages)</span></p>
+      <div><i class="fa fa-star"></i>${rating}/5</div>
+      <span>Tech</span>
+      <span>Programming</span>
+    </div>`
+
+  libraryList.append(bookItem)
+}
+
+printLibraryList()
