65afb8d9fa83386b87a29f82788b6d90f6b59291
Blanca Ley <blanca.ley@outlook.com>
2022-04-12 23:47:49 +0200

    Draw nav bar based on logged in state

--- frontend/js/navigationButtons.js
+++ frontend/js/navigationButtons.js
@@ -1,16 +1,37 @@
-const showLoginBtn = document.getElementById("showLogInBtn");
-const showSignUpNavBtn = document.getElementById("showSignUpNavBtn");
+const navBar = document.getElementById("navBar");
+const navBarBtn = document.getElementById("NavBarBtn");
 const showSignUpFromLoginBtn = document.getElementById("showSignUpFromLoginBtn");
 
-showLoginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  changeActivePage("login");
-})
+const drawNavBarBtn = () => {
+  // Check if user is logged in
+  let isLoggedIn;
+  const token = getToken();
+  if(token) {
+    isLoggedIn = true;
+  } else {
+    isLoggedIn = false;
+  }
 
-showSignUpNavBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  changeActivePage("signup");
-})
+  // Draw nav bar based on logged in state
+  if(isLoggedIn) {
+    drawLoggedInNav()
+  } else {
+    drawPublicNav();
+  }
+}
+
+const drawLoggedInNav = () => {
+  const userProfile = getUserProfile();
+  const {username} = userProfile;
+  navBarBtn.innerHTML =
+  `<button id="loggedInUser" onclick=changeActivePage("profile")><i class="fa fa-user"></i>${username}</button>`
+}
+
+const drawPublicNav = () => {
+  navBarBtn.innerHTML =
+  `<button id="showLogInBtn" onclick=changeActivePage("login")>Log In</button>
+  <button id="showSignUpNavBtn" onclick=changeActivePage("signup");>Sign Up</button>`
+}
 
 showSignUpFromLoginBtn.addEventListener("click", (e) => {
   e.preventDefault();
